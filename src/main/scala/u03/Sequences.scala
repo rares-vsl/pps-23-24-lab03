d95ef61e14f89cfe5d575501c8a267f64c6a52bf
Vasiliu Rares <rares.vasiliu@studio.unibo.it>
2024-03-21 10:54:58 +0100

    init

--- src/main/scala/u03/Sequences.scala
+++ src/main/scala/u03/Sequences.scala
@@ -2,6 +2,7 @@ package u03
 
 import u02.AnonymousFunctions.l
 import u03.Optionals.Optional
+import u03.EncapsulatedSequences.Sequence.nil
 
 object Sequences: // Essentially, generic linkedlists
   
@@ -25,13 +26,36 @@ object Sequences: // Essentially, generic linkedlists
       case Nil()                 => Nil()
 
     // Lab 03
-    def zip[A, B](first: Sequence[A], second: Sequence[B]): Sequence[(A, B)] = ???
-
-    def take[A](l: Sequence[A])(n: Int): Sequence[A] = ???
+    def zip[A, B](first: Sequence[A], second: Sequence[B]): Sequence[(A, B)] = (first, second) match
+       case (Cons(h,t), Cons(h2, t2) ) => Cons((h, h2), zip(t, t2))
+       case _ => Nil()
     
-    def concat[A](l1: Sequence[A], l2: Sequence[A]): Sequence[A] = ???
-    def flatMap[A, B](l: Sequence[A])(mapper: A => Sequence[B]): Sequence[B] = ???
 
+    def take[A](l: Sequence[A])(n: Int): Sequence[A] = (l, n) match
+      case(Cons(h, t), n) if n > 0 => Cons(h, take(t)(n-1))
+      case _ => Nil()
+    
+    def concat[A](l1: Sequence[A], l2: Sequence[A]): Sequence[A] = (l1, l2) match
+      case (Cons(h,t), l2 ) => Cons(h, concat(t, l2))
+      case (Nil(), l2) => l2
+      case _ => Nil()
+    
+
+    def flatMap[A, B](l: Sequence[A])(mapper: A => Sequence[B]): Sequence[B] = l match
+      case Cons(h, t) => concat(mapper(h), flatMap(t)(mapper))
+      case _ => Nil()
+
+    def map2[A, B](l: Sequence[A])(mapper: A => B): Sequence[B] = flatMap(l)( v => Cons(mapper(v), Nil()))
+
+    def filter2[A](l1: Sequence[A])(pred: A => Boolean): Sequence[A] = pred(h) match
+      
+    
+      /*
+       l1 match
+      case Cons(h, t) if pred(h) => Cons(h, filter(t)(pred))
+      case Cons(_, t)            => filter(t)(pred)
+      case Nil()                 => Nil()
+*/
     def min(l: Sequence[Int]): Optional[Int] = ???
     
 @main def trySequences =
